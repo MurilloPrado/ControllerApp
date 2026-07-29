56f198cbb451b24ee76a0aed1c46b6a85862c92d
Murillo Prado <murilloprado1804@gmail.com>
2026-07-29 14:57:48 -0300

    test: add Redux Toolkit setup test files

--- apps/mobile/src/store/index.ts
+++ apps/mobile/src/store/index.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./slices/counterSlice";
 
 // "Database" of the app, where all the data is stored and managed
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
