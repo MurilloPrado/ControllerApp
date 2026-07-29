1408cbd60470fc5251e7d92b89d4f7b36eb62703
Murillo Prado <murilloprado1804@gmail.com>
2026-07-29 15:05:38 -0300

    feat: setup rtk query

--- apps/mobile/src/store/index.ts
+++ apps/mobile/src/store/index.ts
@@ -1,11 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice";
+import { baseApi } from "@/services/api/baseApi";
 
 // "Database" of the app, where all the data is stored and managed
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+        baseApi.middleware
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
