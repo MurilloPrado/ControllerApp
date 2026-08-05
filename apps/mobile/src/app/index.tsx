1128dd867db9506c82f49312b8f464cacc426a6d
Murillo Prado <murilloprado1804@gmail.com>
2026-08-05 16:24:58 -0300

    refactor: reorganize and create new screen structure and component responsibilities

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,22 +1,5 @@
-import { AppLayout } from "@/components/layout/AppLayout";
-import { ActionButtons } from "@/components/media/ActionButtons/ActionPanel";
-import { DirectionPad } from "@/components/media/DirectionPad/DirectionPad";
-import { MediaController } from "@/components/media/MediaController";
-import { Text } from "react-native";
+import { Redirect } from "expo-router";
 
-export default function MediaScreen() {
-  return (
-    <AppLayout
-      title="Multimídia"
-      currentTab="media"
-      onPowerPress={() => console.log("Power")}
-      onSettingsPress={() => console.log("Settings")}
-      onTabChange={(tab) => console.log(tab)}
-    >
-      <Text>Conteúdo da tela</Text>
-      <MediaController/>
-      <DirectionPad />
-      <ActionButtons />
-    </AppLayout>
-  );
+export default function Index() {
+  return <Redirect href="/media" />;
 }
---
+++ apps/mobile/src/app/media.tsx
@@ -0,0 +1,5 @@
+import { MediaScreen } from "@/features/media";
+
+export default function MediaPage() {
+  return <MediaScreen />;
+}
